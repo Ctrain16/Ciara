70180aa812ac9b837d9003970aecaf590e296aa2
Cal Trainor <cal16trainor@gmail.com>
2021-01-06 16:19:55 -0400

    Began implementing music functionality

--- cogs/music.py
+++ cogs/music.py
@@ -1,10 +1,42 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+
+    @commands.command(
+        name='play',
+        description='Plays audio',
+        aliases=['p']
+    )
+    async def join(self,ctx):
+        channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+
+        if voice and voice.is_connected:
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+
+        print(f'Ciara connected to {channel}')
+
+
+    @commands.command(
+        name='disconnect',
+        description='Disconnects Ciara',
+        aliases=['dc']
+    )
+    async def disconnect(self,ctx):
+        user_channel = ctx.message.author.voice.channel
+        voice = get(self.bot.voice_clients, guild = ctx.guild)
+
+        if voice and voice.is_connected and voice.channel == user_channel:
+            await voice.disconnect()
+            print(f'Ciara disconnected from {voice.channel}')
 
 
 def setup(bot):
