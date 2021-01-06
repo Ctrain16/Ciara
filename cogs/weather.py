e7dafc8a7de0610f546afc04423991e78debe2da
Cal Trainor <cal16trainor@gmail.com>
2021-01-06 13:24:47 -0400

    added default location

--- cogs/weather.py
+++ cogs/weather.py
@@ -6,16 +6,31 @@ from discord.ext import commands
 
 import config as ciara
 
+default_location = ''
+
 class Weather(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
+
+    @commands.command(
+        name='location',
+        description='Sets a default location to retrieve the weather for.',
+        aliases=['loc']
+    )
+    async def set_default_location(self,ctx,location):
+        global default_location
+        default_location = location
+        await ctx.send(f'Default location set to {default_location}')
+
+
     @commands.command(
         description='Retrieves the weather for a specified location\n\nlocation should be in city,country abbreviation format.\nSo for example Toronto,Ca',
         aliases=['w']
     )
-    async def weather(self,ctx,location):
+    async def weather(self,ctx,location = None):
+        global default_location
 
         url = 'https://community-open-weather-map.p.rapidapi.com/weather'
 
@@ -25,15 +40,21 @@ class Weather(commands.Cog):
         }
 
         querystring = {
-            'q':location,
+            'q':default_location,
             'units':'metric'
         }
 
-        response = requests.get(url, headers=headers, params=querystring)
-        json_response = json.loads(response.text)
-        
+        if location is not None:
+            querystring['q'] = location
 
-        await ctx.send(json_response['main']['temp'])
+
+        if querystring['q'] == '':
+                await ctx.send('Please specify a location, or set a default location')
+        else:
+            response = requests.get(url, headers=headers, params=querystring)
+            json_response = json.loads(response.text)
+
+            await ctx.send(f'Current temp in ' + querystring['q'] + ' : ' + str(round(json_response['main']['temp'])) + '°C')
 
 
 def setup(bot):
