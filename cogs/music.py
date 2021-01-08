07f8b3f11225ad0e7e0c30d8f85abb1b88ee6e98
Cal Trainor <cal16trainor@gmail.com>
2021-01-08 02:49:15 -0400

    song queue added

--- cogs/music.py
+++ cogs/music.py
@@ -1,9 +1,11 @@
-import os, urllib.parse, urllib.request, re
+import os, urllib.parse, urllib.request, re, shutil
 
 import discord
 from discord.ext import commands
 from discord.utils import get
 import youtube_dl
+
+song_queue = []
 
 class Music(commands.Cog):
 
@@ -11,13 +13,13 @@ class Music(commands.Cog):
         self.bot = bot
         
 
-
     @commands.command(
         name='play',
         description='Plays requested song\n\n<song_request> can either be a url or a title which will be searched for on youtube',
         aliases=['p']
     )
-    async def play(self,ctx,*,song_request):
+    async def play(self,ctx,*,song_request : str = ''):
+        global song_queue
 
         #Functions as resume command when no url is passed
         if song_request == '':
@@ -31,11 +33,30 @@ class Music(commands.Cog):
                 os.remove('song.mp3')
                 print('Music: Removed old song file\n')
         except PermissionError:
-            #Add queue structure... also need to add skip command 
-            print('Music: A new song was requested while the current song is playing')
-            await ctx.send('Music: A song is already playing\n')
+            await ctx.invoke(self.bot.get_command('queue'), song_request=song_request)
             return
 
+        def check_queue():
+            if len(song_queue) > 0:
+                end_of_file_name = song_queue.pop(0) + '.mp3'
+                print(end_of_file_name)
+                os.remove('song.mp3')
+                print('Music: Removed old song file\n')
+
+                #Change file name and retrieves title from file name
+                song_title = ''
+                for file in os.listdir('./'):
+                    if file.endswith(end_of_file_name):
+                        song_title = file
+                        print(f'Music: Renamed file {file} to song.mp3\n')
+                        os.rename(file, 'song.mp3')
+
+                voice.play(
+                    discord.FFmpegPCMAudio('song.mp3'),
+                    after= lambda e: check_queue()
+                )
+
+        
         #Search for song on youtube and get url
         search_query = urllib.parse.urlencode({'search_query': song_request})
         htm_content = urllib.request.urlopen(
@@ -73,8 +94,8 @@ class Music(commands.Cog):
         else:
             voice = await channel.connect()
         voice.play(
-            discord.FFmpegPCMAudio('song.mp3'), 
-            after=lambda e:ctx.invoke(self.bot.get_command('dc'))
+            discord.FFmpegPCMAudio('song.mp3'),
+            after= lambda e: check_queue()
         )
 
         #Prints confirmation
@@ -134,6 +155,41 @@ class Music(commands.Cog):
         voice = get(self.bot.voice_clients, guild = ctx.guild)
         voice.stop()
         print('Music: stopped\n')
+
+    
+    @commands.command(
+        name='queue',
+        aliases=['q']
+    )
+    async def queue(self,ctx,song_request):
+        global song_queue
+
+        print(f'Music: {song_request} added to queue\n')
+        await ctx.send(f'{song_request} was added to the queue\n')
+
+        #Search for song on youtube and get url
+        search_query = urllib.parse.urlencode({'search_query': song_request})
+        htm_content = urllib.request.urlopen(
+            'http://www.youtube.com/results?' + search_query
+        )
+        search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
+        song_url = 'http://www.youtube.com/watch?v=' + search_results[0]
+        print(f'Music: {song_url}')
+
+        song_queue.append(search_results[0])
+        print(f'Music: Queue: {song_queue}\n')
+
+        #Download song
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([song_url])
 
 
 def setup(bot):
