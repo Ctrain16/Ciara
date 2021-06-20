7f9e0fa866b920932eda12fbbc3d17cbe3f917d6
Cal Trainor <cal16trainor@gmail.com>
2021-06-20 15:18:21 -0300

    music 'play' command is working

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -12,7 +12,7 @@ module.exports = class PlayCommand extends Commando.Command {
       group: 'music',
       memberName: 'play',
       description: 'Plays a song',
-      examples: ['play song-name/linke'],
+      examples: ['play song-name/link'],
 
       args: [
         {
@@ -25,31 +25,59 @@ module.exports = class PlayCommand extends Commando.Command {
   }
 
   async run(msg, { song }) {
-    if (!msg.member.voice.channel) {
+    if (!msg.member.voice.channel)
       return msg.reply(
         `You need to be in a voice channel in order to use this command!`
       );
-    }
 
-    const [connection, error] = await this.tryCatchForAsync(
-      msg.member.voice.channel.join()
+    const activeGuildConnection = this.client.voice.connections.get(
+      msg.guild.id
     );
 
-    if (error) {
-      return msg.channel.send(`There was an error joining the voice channel.`);
-    }
+    if (
+      activeGuildConnection &&
+      activeGuildConnection.channel !== msg.member.voice.channel
+    )
+      return msg.reply(
+        `${this.client.user.username} is already bound to \` ${activeGuildConnection.channel.name} \``
+      );
 
-    const songLink = this.validURL(song) ? song : this.searchYoutube(song);
-    console.log(songLink);
-    await this.sleep(1000); // buffer for embed to load
-    console.dir(msg?.embeds);
+    const [connection, error] = activeGuildConnection
+      ? [activeGuildConnection, undefined]
+      : await this.joinVoiceChannel(msg);
+
+    if (error)
+      return msg.channel.send(
+        `There was an error joining \` ${msg.member.voice.channel.name} \``
+      );
+
+    const [songLink, songInfo] = await this.calcSongInformation(song, msg);
     const dispatcher = connection.play(
       ytdl(songLink, { quality: 'highestaudio', filter: 'audioonly' })
     );
 
-    return msg.channel.send(
-      `Joined \` ${msg.member.voice.channel.name} \`, and began playing ${song}`
+    return msg.channel.send(`**Playing >>>** \` ${songInfo.title} \`  🎵`);
+  }
+
+  async joinVoiceChannel(msg) {
+    const [connection, error] = await this.tryCatchForAsync(
+      msg.member.voice.channel.join()
     );
+    if (connection)
+      msg.channel.send(
+        `**Joined >>>** \` ${msg.member.voice.channel.name} \`  👋`
+      );
+    return [connection, error];
+  }
+
+  async calcSongInformation(song, msg) {
+    if (this.validURL(song)) {
+      await this.sleep(500);
+      return [song, msg.embeds[0]];
+    } else {
+      const songInfo = await this.searchYoutube(song);
+      return [songInfo.link, songInfo];
+    }
   }
 
   validURL(str) {
@@ -71,11 +99,6 @@ module.exports = class PlayCommand extends Commando.Command {
     });
   }
 
-  /**
-   * Helper function to help with error handling for async-await
-   * @param {Promise} promise
-   * @returns
-   */
   async tryCatchForAsync(promise) {
     try {
       const data = await promise;
@@ -86,20 +109,18 @@ module.exports = class PlayCommand extends Commando.Command {
     }
   }
 
-  searchYoutube(song) {
+  async searchYoutube(song) {
     const opts = {
       maxResults: 1,
       key: process.env.YOUTUBE_API_KEY,
     };
 
-    return youtubeSearch(song, opts, function (err, results) {
-      if (err) {
-        console.error(error);
-        return undefined;
-      }
-
-      console.dir(results);
-      return results[0].link;
-    });
+    try {
+      const searchResults = await youtubeSearch(song, opts);
+      return searchResults.results[0];
+    } catch (err) {
+      console.error(err);
+      return undefined;
+    }
   }
 };
