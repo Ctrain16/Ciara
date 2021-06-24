641c4c3f06f9c687516f078239baf43c6b22d903
Cal Trainor <cal16trainor@gmail.com>
2021-06-24 00:04:41 -0300

    added level command

---
+++ src/commands/util/level.js
@@ -0,0 +1,37 @@
+const Commando = require('discord.js-commando');
+const { MongoClient } = require('mongodb');
+
+module.exports = class CalculatorCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'level',
+      group: 'util',
+      memberName: 'level',
+      description: 'Checks your server level.',
+    });
+  }
+
+  async run(msg) {
+    const mongoClient = new MongoClient(process.env.MONGO_CONNECTION, {
+      useUnifiedTopology: true,
+    });
+    await mongoClient.connect();
+    const serverLevelsCollection = mongoClient
+      .db('ciaraDataBase')
+      .collection('serverlevels');
+
+    if (!serverLevelsCollection)
+      throw new Error('Failed to find database and/or collection');
+
+    const userLevelDoc = await serverLevelsCollection.findOne({
+      authorId: msg.author.id,
+      guildId: msg.guild.id,
+    });
+
+    return msg.reply(
+      !userLevelDoc
+        ? `Congrats on your first message here! You're **Level: 0**`
+        : `you are **Level: ${userLevelDoc.level}**`
+    );
+  }
+};
