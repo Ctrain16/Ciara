Ctrain16/Ciara

commit 82e40bff304a16df30c4eb1a835745c5b8e2f79c

increase farm penalty

src/util/level-runner.js
@@ -36,6 +36,9 @@ const FARM_RELATED_WORDS = [
   'eggs',
   'cattle',
 ];
+
+const FARM_PENALTY = -5000;
+
 const _awardRoleToUser = async function (client, msg, newLevel) {
   const member = msg.member;
   let levelRoles = client.provider.get(msg.guild.id, 'levelRoles');
@@ -97,9 +100,9 @@ const _updateUserLevel = async function (mongoClient, msg, client) {
     });
     return;
   } else if (_isFarmMessage(msg)) {
-    updateValue = -updateValue;
+    updateValue = -FARM_PENALTY;
     await msg.reply(
-      `👩‍🌾 Quit farming ${msg.channel} you noob 👨‍🌾 ... also you lost a point for this.`
+      `👩‍🌾 Quit farming ${msg.channel} you noob 👨‍🌾 ... also you 50 points for this peasant. #SkillIssue #SoundsMad`
     );
   } else if (_isSpamMessage(userLevelDoc)) {
     return; // don't aware points for spam messages

commit efaeeaaa0ba4c18f409d9e9313b86f84254b803a

fixed bug introduced in 82e40bf where farm penalty was positive and decreased penalty

src/util/level-runner.js
@@ -37,7 +37,7 @@ const FARM_RELATED_WORDS = [
   'cattle',
 ];
 
-const FARM_PENALTY = -5000;
+const FARM_PENALTY = -1000;
 
 const _awardRoleToUser = async function (client, msg, newLevel) {
   const member = msg.member;
@@ -100,9 +100,9 @@ const _updateUserLevel = async function (mongoClient, msg, client) {
     });
     return;
   } else if (_isFarmMessage(msg)) {
-    updateValue = -FARM_PENALTY;
+    updateValue = FARM_PENALTY;
     await msg.reply(
-      `👩‍🌾 Quit farming ${msg.channel} you noob 👨‍🌾 ... also you 50 points for this peasant. #SkillIssue #SoundsMad`
+      `👩‍🌾 Quit farming ${msg.channel} you noob 👨‍🌾 ... also you lost some points for this peasant. #SkillIssue #SoundsMad`
     );
   } else if (_isSpamMessage(userLevelDoc)) {
     return; // don't aware points for spam messages