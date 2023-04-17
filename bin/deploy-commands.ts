5954afb63e8321ba593397c4ca9bd76909f465d4
Cal Trainor <cal16trainor@gmail.com>
2023-04-16 17:49:44 -0700

    update deploy commands script to be global scoped

--- bin/deploy-commands.ts
+++ bin/deploy-commands.ts
@@ -2,8 +2,7 @@ import { REST, Routes } from 'discord.js';
 import { commands } from '../src/commands';
 import EnvValueProvider from '../src/util/EnvValueProvider';
 
-const guildId = process.argv[2];
-const isProdEnv: boolean = process.argv[3] === 'prod';
+const isProdEnv: boolean = process.argv[2] === 'prod';
 const envValueProvider = new EnvValueProvider(isProdEnv);
 
 const commandsJson = commands.map((command) => command.data.toJSON());
@@ -13,14 +12,11 @@ const rest = new REST().setToken(envValueProvider.getBotToken());
 (async () => {
   try {
     console.log(
-      `Started refreshing ${commandsJson.length} application (/) commands in guildId: ${guildId}`
+      `Started refreshing ${commandsJson.length} application (/) globally (include DM's).`
     );
 
     const data: any = await rest.put(
-      Routes.applicationGuildCommands(
-        envValueProvider.getBotClientId(),
-        guildId
-      ),
+      Routes.applicationCommands(envValueProvider.getBotClientId()),
       { body: commandsJson }
     );
 
